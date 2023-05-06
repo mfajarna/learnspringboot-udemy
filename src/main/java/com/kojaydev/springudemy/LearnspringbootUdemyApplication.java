c2bddeaf8f3ea74c8f2303117d74f16a82ecb6c1
muhammad fajar <invasionfajar@gmail.com>
2023-05-06 11:16:10 +0700

    Initial commit

---
+++ src/main/java/com/kojaydev/springudemy/LearnspringbootUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.kojaydev.springudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbootUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnspringbootUdemyApplication.class, args);
+    }
+
+}
